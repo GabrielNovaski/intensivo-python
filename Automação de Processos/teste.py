f3c63b5d12bfa5d16caebaa7d39fdec1fd38a8c4
GabrielNovaski <gabrielnovaski5@gmail.com>
2024-08-19 14:36:59 -0300

    Update teste.py

--- Automação de Processos/teste.py
+++ Automação de Processos/teste.py
@@ -1,8 +1,1 @@
-import pyautogui
-from time import sleep
-import pandas as pd
 
-tabela = pd.read_csv(r"C:\Users\Ghn12\OneDrive\Documentos\Python\hashtag\produtos.csv")
-
-for linha in tabela:
-    print(linha)
